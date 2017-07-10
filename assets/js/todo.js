4d08c9d69fc75842660d7898081c288992029665
asmm.dan <asmm.dan@gmail.com>
2017-07-09 19:49:52 -0700

    to do list

---
+++ assets/js/todo.js
@@ -0,0 +1,20 @@
+//$("ul").on("click","li", function(){
+//    $(this).toggleClass("completed");
+//});
+$("ul").on("click","span", function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+    
+});
+
+$("input[type='text'").on("keypress",function(event){
+   if(event.which === 13){
+       $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + $(this).val() + "</li>" );
+   }
+});
+
+$(".fa-plus").click(function(){
+  $("input[type='text'").fadeToggle();
+});
